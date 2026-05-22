910ca59d1b2690d26fdf1dfaae3397b6a4b47dec
puneet-kumar-garg <puneetgarg551@gmail.com>
2026-05-22 22:45:23 +0530

    refactor: add cn utility helper for classnames

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// utils v2
+// utils v3
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
