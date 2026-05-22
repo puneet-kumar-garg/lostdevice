a2ace6a9b5595b374a21bc2542127db5ce804b95
puneet-kumar-garg <puneetgarg551@gmail.com>
2026-05-22 22:53:40 +0530

    feat: add edge function for secure device search

--- supabase/functions/search-devices/index.ts
+++ supabase/functions/search-devices/index.ts
@@ -1,4 +1,4 @@
-// search edge function v2
+// search edge function v3
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.38.4';
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 
