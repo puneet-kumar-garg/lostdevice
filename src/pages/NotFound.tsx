36208e661bec0b504ce51e1fb736723a9db5072e
puneet-kumar-garg <puneetgarg551@gmail.com>
2026-05-22 22:37:59 +0530

    feat: add 404 not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-// not found page v2
+// not found page v3
 const NotFound = () => {
   const location = useLocation();
 
